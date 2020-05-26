f07fd2ae12552a24fd35e93bfbb784a051baa767
Masayoshi Mizutani <mizutani@cookpad.com>
2020-05-26 10:23:13 +0900

    Add CDK files

---
+++ test/aws-alarm-notify.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsAlarmNotify from '../lib/aws-alarm-notify-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AwsAlarmNotify.AwsAlarmNotifyStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
